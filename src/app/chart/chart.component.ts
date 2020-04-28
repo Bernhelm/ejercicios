9d0db5caae3fbfc5ac9277796f5eeae645603d59
Alberto García <cernhelm@gmail.com>
2020-04-28 19:21:16 +0200

    Agregadas graficas genericas

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -15,6 +15,27 @@ export class ChartComponent {
   lineChartLabels: Label[];
   lineChartOptions = {
     responsive: true,
+    scale: {
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 100,
+      },
+    },
+  };
+
+  lineChartOptionsLine = {
+    responsive: true,
+    scales: {
+      yAxes: [
+        {
+          display: true,
+          ticks: {
+            suggestedMin: 0, // minimum value will be 0.
+            suggestedMax: 100, // minimum value will be 0.
+          },
+        },
+      ],
+    },
   };
 
   lineChartColors: Color[] = [
@@ -28,6 +49,8 @@ export class ChartComponent {
   lineChartPlugins = [];
   lineChartType = 'radar';
 
+  charts;
+
   constructor(
     private DataService: DataService,
     private SelectorService: SelectorService
@@ -38,6 +61,38 @@ export class ChartComponent {
     this.lineChartData = data.map((userData) => {
       const data = userData.data.map((row) => row.slice(-1).pop());
       return { data, label: userData.label };
+    });
+
+    this.charts = exercises.map((exercise) => {
+      const chart = {
+        name: exercise.name,
+        data: [],
+        labels: [],
+      };
+
+      const maxNumberValues: number = data.reduce((accum, userData) => {
+        return Math.max(accum, userData.data[exercise.level].length);
+      }, 0);
+
+      chart.data = data.map((userData) => {
+        let singleData = userData.data[exercise.level];
+        const length = singleData.length;
+        if (length < maxNumberValues) {
+          singleData = [
+            ...singleData,
+            ...new Array(maxNumberValues - length).fill(singleData[length - 1]),
+          ];
+        }
+        let dataSet: ChartDataSets = {
+          data: singleData,
+          label: userData.label,
+        };
+        return dataSet;
+      });
+
+      chart.labels = new Array(maxNumberValues).fill('');
+
+      return chart;
     });
   }
 }
